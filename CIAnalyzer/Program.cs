68d57fa469c47556df2401c91584a702f0b9395b
lissettecid <lissettecidm@gmail.com>
2022-07-01 17:04:09 -0400

    Add project files.

---
+++ CIAnalyzer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CIAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(args[1]);
+            int num2 = int.Parse(args[2]);
+
+            int sum = Sum(num1, num2);
+            decimal div = Divide(num1, num2);
+        }
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Divide(int num1, int num2)
+        {
+            return num1 / num2;
+
+        }
+    }
+}
